83ce87cd640a5c696441cc9b68cbc6ed38910a3c
Rafa Gómez <rgomezcasas@gmail.com>
2019-07-01 11:54:37 +0200

    [05.1] Add `/authors/?course_id=%course_id%` endpoint

--- 05-requests-and-responses/05.1-splitting-in-multiples-endpoints/monolito/controller/CoursesGetController.php
+++ 05-requests-and-responses/05.1-splitting-in-multiples-endpoints/monolito/controller/CoursesGetController.php
@@ -4,14 +4,14 @@ declare(strict_types = 1);
 
 final class CoursesGetController extends Controller
 {
-    private const ALL_COURSES = [
+    public const ALL_COURSES = [
         77070 => [
             'id'        => '77070',
             'title'     => 'Principios SOLID aplicados',
             'thumbnail' => 'https://pathwright.imgix.net/https%3A%2F%2Fcdn.filestackcontent.com%2Fapi%2Ffile%2FLfssR9kbRqyx2LaCMxp5%3Fsignature%3D888b9ea3eb997a4d59215bfbe2983c636df3c7da0ff8c6f85811ff74c8982e34%26policy%3DeyJjYWxsIjogWyJyZWFkIiwgInN0YXQiLCAiY29udmVydCJdLCAiZXhwaXJ5IjogNDYyMDM3NzAzMX0%253D?dpr=2&fit=crop&h=232&ixlib=python-1.1.0&w=310&s=b387f292e2d8ac9cd4509923c3148971',
             'url'       => 'https://pro.codely.tv/library/principios-solid-aplicados/77070/about/',
             'authors'   => ['b4b9ee99-8290-4064-8e3c-e4fd925fe0b9', '39402ea6-d5a0-4466-918d-e5eef75cc8a9'],
-        ],
+            ],
         66748 => [
             'id'        => '66748',
             'title'     => 'Arquitectura Hexagonal',
