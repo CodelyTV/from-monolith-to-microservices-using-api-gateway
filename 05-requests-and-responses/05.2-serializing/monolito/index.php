fc8c178918f680f9d0b2da58f3d8b027ab48beb4
Rafa Gómez <rgomezcasas@gmail.com>
2019-07-01 11:57:14 +0200

    [05.2] Add base monolith

---
+++ 05-requests-and-responses/05.2-serializing/monolito/index.php
@@ -0,0 +1,42 @@
+<?php
+
+require __DIR__ . '/auth/AuthHelper.php';
+require __DIR__ . '/auth/SessionAuth.php';
+
+require __DIR__ . '/controller/Controller.php';
+require __DIR__ . '/controller/AuthorsGetController.php';
+require __DIR__ . '/controller/CoursesGetController.php';
+require __DIR__ . '/controller/PathsGetController.php';
+
+require __DIR__ . '/middleware/AuthMiddleware.php';
+
+$url      = strtok($_SERVER['REQUEST_URI'], '?');
+$urlPath  = explode('/', $url)[1];
+$response = '';
+$request  = array_merge($_REQUEST, ['REQUEST_URL' => $url, 'REQUEST_URL_PATH' => $urlPath]);
+
+if ($_POST) {
+    $response = "The POST `$url` doesn't exist!";
+} else {
+    switch ($urlPath) {
+        case '':
+            break;
+        case 'authors':
+            $response = (new AuthMiddleware(new AuthorsGetController(), new SessionAuth()))->handle($request);
+
+            break;
+        case 'courses':
+            $response = (new AuthMiddleware(new CoursesGetController(), new SessionAuth()))->handle($request);
+
+            break;
+        case 'paths':
+            $response = (new AuthMiddleware(new PathsGetController(), new SessionAuth()))->handle($request);
+
+            break;
+        default:
+            $response = "The GET `$url` doesn't exist!";
+            break;
+    }
+}
+
+echo $response;
