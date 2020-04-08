9ddf00599f02abfdfafe48e10e5c16ac7a3409d5
Albert Lombarte <alombarte@gmail.com>
2020-04-08 19:37:32 +0200

    Example curls can be executed at once

--- 01-monolith/example-curls.sh
+++ 01-monolith/example-curls.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 
-curl -iXPOST -F'username=dani' -F'pwd=supu' http://127.0.0.1:8081/login
+# Login and capture of cookie value from the `Set-Cookie`header:
+echo "Logging in..."
+COOKIE=$(curl -s -iXPOST -F'username=dani' -F'pwd=supu' http://127.0.0.1:8081/login | grep Set-Cookie | cut -d' ' -f2-)
 
-curl -iH'Cookie: PHPSESSID=cc46b992e369d8e4df50e120aeee1eab; path=/' "http://127.0.0.1:8081/courses?course_id=77070"
-curl -iH'Cookie: PHPSESSID=cc46b992e369d8e4df50e120aeee1eab; path=/' http://127.0.0.1:8081/logout
+# Inject the cookie in the following responses:
+echo "Retrieving protected endpoint"
+curl -iH"Cookie: $COOKIE" "http://127.0.0.1:8081/courses?course_id=77070"
+
+# Bye!
+echo "Logging out"
+curl -iH"Cookie: $COOKIE" http://127.0.0.1:8081/logout
